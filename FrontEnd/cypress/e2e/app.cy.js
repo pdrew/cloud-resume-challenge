3d61bed4366cde2ea3ffa6249cbd534d79d561a3
pdrew <pdrew@tessituranetwork.com>
2023-04-06 21:12:11 +1000

    Adding smoke test to GitHub Actions

--- FrontEnd/cypress/e2e/app.cy.js
+++ FrontEnd/cypress/e2e/app.cy.js
@@ -1,21 +1,23 @@
 describe('resume site', () => {
 
+  const url = Cypress.env('RESUME_SITE_URL');
+
   Cypress.config('defaultCommandTimeout', 30000);
 
   it('displays total views', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit(url);
 
-    cy.get('#total-views').invoke('text').then(parseFloat).should('be.gt', 0)
+    cy.get('#total-views').invoke('text').then(parseFloat).should('be.gt', 0);
   }),
   it('increments total views', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit(url);
 
     cy.get('#total-views').invoke('text').then((text) => {
-      const totalViews = parseFloat(text)
+      const totalViews = parseFloat(text);
 
-      cy.visit('http://localhost:3000')
+      cy.visit(url);
 
-      cy.get('#total-views').invoke('text').then(parseFloat).should('be.gt', totalViews)
-    })
+      cy.get('#total-views').invoke('text').then(parseFloat).should('be.gt', totalViews);
+    });
   })
 })
