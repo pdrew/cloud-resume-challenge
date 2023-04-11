4b9147414e4bdf05dbdab956ee7bca7e94378aa8
pdrew <patrick.r.drew@gmail.com>
2023-04-11 20:39:50 +1000

    Adding lambda code signing

--- Build/src/BackEnd.cs
+++ Build/src/BackEnd.cs
@@ -8,6 +8,7 @@ using Amazon.CDK.AWS.Lambda;
 using Amazon.CDK.AWS.Logs;
 using Amazon.CDK.AWS.Route53;
 using Amazon.CDK.AWS.Route53.Targets;
+using Amazon.CDK.AWS.Signer;
 using Constructs;
 using AssetOptions = Amazon.CDK.AWS.S3.Assets.AssetOptions;
 
@@ -26,114 +27,125 @@ public class BackEnd : Construct
         });
         
         var table = new Table(this, "CloudResumeChallengeDatabase", new TableProps()
+        {
+            TableName = "CloudResumeChallengeDatabase",
+            PartitionKey = new Attribute()
             {
-                TableName = "CloudResumeChallengeDatabase",
-                PartitionKey = new Attribute()
-                {
-                    Name = "pk",
-                    Type = AttributeType.STRING
-                },
-                RemovalPolicy = RemovalPolicy.DESTROY
-            });
+                Name = "pk",
+                Type = AttributeType.STRING
+            },
+            RemovalPolicy = RemovalPolicy.DESTROY
+        });
+
+        var signingProfile = new SigningProfile(this, "SigningProfile", new SigningProfileProps()
+        {   
+            Platform = Platform.AWS_LAMBDA_SHA384_ECDSA
+        });
+
+        var signingConfig = new CodeSigningConfig(this, "CodeSigningConfig", new CodeSigningConfigProps()
+        {
+            SigningProfiles = new [] { signingProfile }
+        });
             
-            var lambdaFunction = new Function(this, "CloudResumeChallengeLambdaFunction", new FunctionProps()
-            {
-                Runtime = Runtime.DOTNET_6,
-                MemorySize = 256,
-                LogRetention = RetentionDays.ONE_DAY,
-                Handler = "BackEnd",
-                Timeout = Duration.Seconds(30),
-                Code = GetFunctionCode(useDockerBundling)
-            });
+        var lambdaFunction = new Function(this, "CloudResumeChallengeLambdaFunction", new FunctionProps()
+        {
+            Runtime = Runtime.DOTNET_6,
+            MemorySize = 256,
+            LogRetention = RetentionDays.ONE_DAY,
+            Handler = "BackEnd",
+            Timeout = Duration.Seconds(30),
+            Code = GetFunctionCode(useDockerBundling),
+            CodeSigningConfig = signingConfig
+        });
             
-            lambdaFunction.AddToRolePolicy(new PolicyStatement(new PolicyStatementProps()
+        lambdaFunction.AddToRolePolicy(new PolicyStatement(new PolicyStatementProps()
+        {
+            Sid = "DynamoDBIndexAndStreamAccess",
+            Effect = Effect.ALLOW,
+            Actions = new []
             {
-                Sid = "DynamoDBIndexAndStreamAccess",
-                Effect = Effect.ALLOW,
-                Actions = new []
-                {
-                    "dynamodb:GetShardIterator",
-                    "dynamodb:Scan",
-                    "dynamodb:Query",
-                    "dynamodb:DescribeStream",
-                    "dynamodb:GetRecords",
-                    "dynamodb:ListStreams"
-                },
-                Resources = new []
-                {
-                    table.TableArn,
-                    $"{table.TableArn}/stream/*"
-                }
-            }));
+                "dynamodb:GetShardIterator",
+                "dynamodb:Scan",
+                "dynamodb:Query",
+                "dynamodb:DescribeStream",
+                "dynamodb:GetRecords",
+                "dynamodb:ListStreams"
+            },
+            Resources = new []
+            {
+                table.TableArn,
+                $"{table.TableArn}/stream/*"
+            }
+        }));
             
-            lambdaFunction.AddToRolePolicy(new PolicyStatement(new PolicyStatementProps()
+        lambdaFunction.AddToRolePolicy(new PolicyStatement(new PolicyStatementProps()
+        {
+            Sid = "DynamoDBTableAccess",
+            Effect = Effect.ALLOW,
+            Actions = new []
             {
-                Sid = "DynamoDBTableAccess",
-                Effect = Effect.ALLOW,
-                Actions = new []
-                {
-                    "dynamodb:BatchGetItem",
-                    "dynamodb:BatchWriteItem",
-                    "dynamodb:ConditionCheckItem",
-                    "dynamodb:PutItem",
-                    "dynamodb:DescribeTable",
-                    "dynamodb:DeleteItem",
-                    "dynamodb:GetItem",
-                    "dynamodb:Scan",
-                    "dynamodb:Query",
-                    "dynamodb:UpdateItem"
-                },
-                Resources = new []
-                {
-                    table.TableArn
-                }
-            }));
+                "dynamodb:BatchGetItem",
+                "dynamodb:BatchWriteItem",
+                "dynamodb:ConditionCheckItem",
+                "dynamodb:PutItem",
+                "dynamodb:DescribeTable",
+                "dynamodb:DeleteItem",
+                "dynamodb:GetItem",
+                "dynamodb:Scan",
+                "dynamodb:Query",
+                "dynamodb:UpdateItem"
+            },
+            Resources = new []
+            {
+                table.TableArn
+            }
+        }));
             
-            lambdaFunction.AddToRolePolicy(new PolicyStatement(new PolicyStatementProps()
+        lambdaFunction.AddToRolePolicy(new PolicyStatement(new PolicyStatementProps()
+        {
+            Sid = "DynamoDBDescribeLimitsAccess",
+            Effect = Effect.ALLOW,
+            Actions = new []
             {
-                Sid = "DynamoDBDescribeLimitsAccess",
-                Effect = Effect.ALLOW,
-                Actions = new []
-                {
-                    "dynamodb:DescribeLimits"
-                },
-                Resources = new []
-                {
-                    table.TableArn,
-                    $"{table.TableArn}/stream/*"
-                }
-            }));
+                "dynamodb:DescribeLimits"
+            },
+            Resources = new []
+            {
+                table.TableArn,
+                $"{table.TableArn}/stream/*"
+            }
+        }));
             
-            var api = new LambdaRestApi(this, "CloudResumeChallengeApi", new LambdaRestApiProps()
-            {
-               Handler = lambdaFunction,
-               Proxy = true,
-               DomainName = new DomainNameOptions()
+        var api = new LambdaRestApi(this, "CloudResumeChallengeApi", new LambdaRestApiProps()
+        {
+           Handler = lambdaFunction,
+           Proxy = true,
+           DomainName = new DomainNameOptions()
+           {
+               DomainName = subdomainName,
+               Certificate = certificate
+           },
+           DeployOptions = new StageOptions()
+           {
+               MethodOptions = new Dictionary<string, IMethodDeploymentOptions>()
                {
-                   DomainName = subdomainName,
-                   Certificate = certificate
-               },
-               DeployOptions = new StageOptions()
-               {
-                   MethodOptions = new Dictionary<string, IMethodDeploymentOptions>()
                    {
+                       "/*/*", new MethodDeploymentOptions()
                        {
-                           "/*/*", new MethodDeploymentOptions()
-                           {
-                               ThrottlingBurstLimit = 1000,
-                               ThrottlingRateLimit = 10
-                           }
+                           ThrottlingBurstLimit = 1000,
+                           ThrottlingRateLimit = 10
                        }
                    }
                }
-            });
+           }
+        });
             
-            new ARecord(this, "CloudResumeChallengeBackEndARecord", new ARecordProps()
-            {
-                RecordName = "resume-api",
-                Zone = zone,
-                Target = RecordTarget.FromAlias(new ApiGateway(api))
-            });
+        new ARecord(this, "CloudResumeChallengeBackEndARecord", new ARecordProps()
+        {
+            RecordName = "resume-api",
+            Zone = zone,
+            Target = RecordTarget.FromAlias(new ApiGateway(api))
+        });
     }
     
     private Code GetFunctionCode(bool useDockerBundling)
