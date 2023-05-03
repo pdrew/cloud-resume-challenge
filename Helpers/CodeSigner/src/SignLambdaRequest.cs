af2a1c4831b2a52c5319ad49b4ecc46ec62c4885
pdrew <patrick.r.drew@gmail.com>
2023-05-03 19:12:10 +1000

    Adding missing files

---
+++ Helpers/CodeSigner/src/SignLambdaRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeSigner;
+
+public class SignLambdaRequest
+{
+    public string ProfileName { get; set; }
+    public string BucketName { get; set; }
+    public string ObjectKey { get; set; }
+    
+    public string TimeStamp { get; set; }
+}
