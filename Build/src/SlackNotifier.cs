b60591c27ac6a4e3d296de477e6bd89e1ca14037
pdrew <patrick.r.drew@gmail.com>
2023-05-03 20:12:59 +1000

    Wiring up SlackNotifier

---
+++ Build/src/SlackNotifier.cs
@@ -0,0 +1,29 @@
+﻿using Amazon.CDK;
+using Amazon.CDK.AWS.Lambda;
+using Amazon.CDK.AWS.Lambda.EventSources;
+using Amazon.CDK.AWS.Logs;
+using Amazon.CDK.AWS.SNS;
+using Constructs;
+
+namespace Build;
+
+public class SlackNotifier  : Construct
+{
+    public SlackNotifier(Construct scope, string id, Topic topic) : base(scope, id)
+    {
+        var function = new Function(this, "Function", new FunctionProps()
+        {
+            Runtime = Runtime.DOTNET_6,
+            MemorySize = 256,
+            LogRetention = RetentionDays.ONE_DAY,
+            Handler = "SlackNotifier::SlackNotifier.Function::FunctionHandler",
+            Timeout = Duration.Seconds(30),
+            Code = Code.FromAsset("../Helpers/SlackNotifier/dist/slacknotifier-function.zip"),
+            Description = "SlackNotifierFunction"
+        });
+
+        var eventSource = new SnsEventSource(topic);
+        
+        function.AddEventSource(eventSource);
+    }
+}
---
+++ Helpers/SlackNotifier/src/SlackNotifier.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents;
+
+namespace SlackNotifier;
+
+public class SlackNotifier
+{
+    public async Task Handle(SNSEvent @event, ILambdaContext context)
+    {
+        foreach (var record in @event.Records)
+        {
+            context.Logger.LogInformation($"Processed record {record.Sns.Message}");
+            
+            // TODO: Do interesting work based on the new message
+            await Task.CompletedTask;
+        }
+    }
+}
